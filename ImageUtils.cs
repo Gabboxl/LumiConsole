d47018f5158af5f7a27733e2ec8c6d35ffa6163f
Gabriele <gabboxl0@gmail.com>
2023-08-07 15:59:03 +0200

    Make CalculateLuminosity() and AdjustLuminosity() faster
    by using the Bitmap.LockBits system

--- ImageUtils.cs
+++ ImageUtils.cs
@@ -1,4 +1,6 @@
 ﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace LumiConsole
 {
@@ -12,19 +14,27 @@ namespace LumiConsole
         public double CalculateLuminosity(Bitmap bitmap)
         {
             double luminosity = 0;
-            int pixelCount = 0;
-            for (int y = 0; y < bitmap.Height; y++)
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+            int pixelCount = width * height;
+
+            var rect = new Rectangle(0, 0, width, height);
+            var data = bitmap.LockBits(rect, ImageLockMode.ReadOnly, bitmap.PixelFormat);
+            var depth = Image.GetPixelFormatSize(data.PixelFormat) / 8; //bytes per pixel
+            var buffer = new byte[width * height * depth];
+
+            Marshal.Copy(data.Scan0, buffer, 0, buffer.Length);
+            bitmap.UnlockBits(data);
+
+            for (int i = 0; i < buffer.Length; i += depth)
             {
-                for (int x = 0; x < bitmap.Width; x++)
-                {
-                    Color pixel = bitmap.GetPixel(x, y);
-                    double b = Linearize(pixel.B / 255.0);
-                    double g = Linearize(pixel.G / 255.0);
-                    double r = Linearize(pixel.R / 255.0);
-                    luminosity += 0.2126 * r + 0.7152 * g + 0.0722 * b;
-                    pixelCount++;
-                }
+                double b = Linearize(buffer[i] / 255.0);
+                double g = Linearize(buffer[i + 1] / 255.0);
+                double r = Linearize(buffer[i + 2] / 255.0);
+
+                luminosity += 0.2126 * r + 0.7152 * g + 0.0722 * b;
             }
+
             return luminosity / pixelCount;
         }
 
@@ -37,23 +47,51 @@ namespace LumiConsole
         {
             double currentLuminosity = CalculateLuminosity(bitmap);
             double adjustFactor = targetLuminosity / currentLuminosity;
-            for (int y = 0; y < bitmap.Height; y++)
+
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            int bytesPerPixel = Image.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            int heightInPixels = data.Height;
+            int widthInBytes = data.Width * bytesPerPixel;
+            byte[] pixels = new byte[data.Height * data.Stride];
+
+            Marshal.Copy(data.Scan0, pixels, 0, pixels.Length);
+
+
+            for (int y = 0; y < heightInPixels; y++)
             {
-                for (int x = 0; x < bitmap.Width; x++)
+                int currentLine = y * data.Stride;
+                for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
                 {
-                    Color pixel = bitmap.GetPixel(x, y);
-                    double b = Linearize(pixel.B / 255.0);
-                    double g = Linearize(pixel.G / 255.0);
-                    double r = Linearize(pixel.R / 255.0);
+                    int bIndex = currentLine + x;
+                    int gIndex = bIndex + 1;
+                    int rIndex = bIndex + 2;
+
+
+                    double b = Linearize(pixels[bIndex] / 255.0);
+                    double g = Linearize(pixels[gIndex] / 255.0);
+                    double r = Linearize(pixels[rIndex] / 255.0);
+
+
                     b = b * adjustFactor;
                     g = g * adjustFactor;
                     r = r * adjustFactor;
+
+
                     b = Math.Min(1, GammaCorrect(b));
                     g = Math.Min(1, GammaCorrect(g));
                     r = Math.Min(1, GammaCorrect(r));
-                    bitmap.SetPixel(x, y, Color.FromArgb((byte)(r * 255), (byte)(g * 255), (byte)(b * 255)));
+
+
+
+                    pixels[rIndex] = (byte)(r * 255);
+                    pixels[gIndex] = (byte)(g * 255);
+                    pixels[bIndex] = (byte)(b * 255);
                 }
             }
+            
+
+            Marshal.Copy(pixels, 0, data.Scan0, pixels.Length);
+            bitmap.UnlockBits(data);
             return bitmap;
         }
     }

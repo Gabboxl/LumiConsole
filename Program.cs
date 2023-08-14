a623e5eddbbe432a25979476285b97a123a81c62
Gabriele <gabboxl0@gmail.com>
2023-08-14 17:25:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,20 +6,33 @@ namespace LumiConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to LumiConsole!");
 
-            //load image as bitmap
-            Bitmap bitmap = new Bitmap("C:\\Users\\Gabriele\\Desktop\\tony.jpg");
 
-            //calculate luminosity
-            //var luminosity = new ImageUtils().CalculateLuminosity(bitmap);
+            //define a folder and interate for every image
+            string[] files = Directory.GetFiles("C:\\Users\\Gabriele\\source\\repos\\FluidWeather\\FluentWeather\\Assets\\bgs", "*.jpg");
+            foreach (string file in files)
+            {
+                //adjust luminosity for every image and save it with the same name but in a different folder
+                Bitmap bitmap2 = new Bitmap(file);
 
-            //Console.WriteLine("triplosette ent: " + luminosity );
+                //calculate luminosity and print it in console with the name of the image
+                var luminosity = new ImageUtils().CalculateLuminosity(bitmap2);
+                //Console.WriteLine(Path.GetFileName(file) + " " + luminosity);
 
-            var newbitmap = new ImageUtils().AdjustLuminosity(bitmap, 0.20);
+                if (luminosity > 0.15)
+                {
+                    //take input luminosity from the user
+                    Console.WriteLine("Insert the luminosity you want to set for the image " + Path.GetFileName(file) + " (" + luminosity + ")");
+                    double targetLuminosity = Convert.ToDouble(Console.ReadLine());
 
-            newbitmap.Save("C:\\Users\\Gabriele\\Desktop\\tony3.jpg");
 
+                    
+                    var newbitmap = new ImageUtils().AdjustLuminosity(bitmap2, targetLuminosity);
+                    newbitmap.Save("C:\\Users\\Gabriele\\Desktop\\newimgfixed\\" + Path.GetFileName(file));
+                }
+
+            }
         }
     }
 }
